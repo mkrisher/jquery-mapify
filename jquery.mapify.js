95bafa86cf5db29f9045091c19d21c8524bca3b6
Michael Krisher <mike@mikekrisher.com>
2009-01-06 19:31:29 -0500

    added logic to center on zoom, and refactoring

--- jquery.mapify.js
+++ jquery.mapify.js
@@ -11,7 +11,9 @@
  * http://www.gnu.org/licenses/gpl.html
  * 
  * TODO: add logic for overwriting defaults with options passed in
- * TODO: adding zoom in and zoom out logic, making it as easy as possible to add multiple resolutions of the "map" for zoom levels
+ * TODO: refactor add zoom_levels, make dynamic based on image_sizes count
+ * TODO: enable zoom scale (track)
+ * TODO: refactor zoom property to start at 0
  */
 /**
  *
@@ -21,17 +23,47 @@
  * 
  * @param Object obj          An object literal containing key/value pairs to overwrite default settings.
  * 
- * @option String image 	    A string of the image path as the basis for the map. 
+ * @option String map_image 	A string of the base naming convention for the map image(s). 
  * 												    Default value: "map.gif"
  * 
- * @option Number zoom 		    A number between 100-200. 100 completely zoomed out. 200 being the most zoomed in. 
- * 												    Default value: 100
+ * @option String up_image 	  A string of the image path for the up button image. 
+ * 												    Default value: "up.gif"
+ * 
+ * @option String right_image A string of the image path for the right button image. 
+ * 												    Default value: "right.gif"
  *
- * @option Number coordinates An array containing the x and y position of the map to initially jump to.
- * 												    Default value: [0,0]
+ * @option String down_image 	A string of the image path for the down button image. 
+ * 												    Default value: "down.gif"
  *
- * @option String image 			A string of the image path as the basis for the map. 
- * 												    Default value: "map.gif"
+ * @option String left_image 	A string of the image path for the left button image. 
+ * 												    Default value: "left.gif"
+ *
+ * @option String center_image A string of the image path for the center button image. 
+ * 												    Default value: "center.gif"
+ *
+ * @option String plus_image 	A string of the image path for the plus button image. 
+ * 												    Default value: "plus.gif"
+ *
+ * @option String minus_image A string of the image path for the minus button image. 
+ * 												    Default value: "minus.gif"
+ *
+ * @option String track_image A string of the image path for the track button image (zoom scale). 
+ * 												    Default value: "track.gif"
+ *
+ * @option String slider_image A string of the image path for the slider button image (zoom handle). 
+ * 												    Default value: "slider.gif"
+ *
+ * @option Number zoom 		    An integer representing the image in the images array to start with (starts at 1). 
+ * 												    Default value: 1
+ *
+ * @option Array zoom_sizes 	An array of the resolutions of the various map images. 
+ * 												    Default value: [[800,600], [1600,1200], [3200,2400]]
+ *
+ * @option Array coordinates  An array containing the x and y position of the map to initially center on.
+ * 												    Default value: [200,200]
+ *
+ * @option Integer distance 	An integer of the pixels the map should move when directional button is clicked. 
+ * 												    Default value: 256
  *
  * @option Boolean debug 			A boolean to turn debugging messages on and off 
  * 												    Default value: true
@@ -56,29 +88,29 @@
             // ------------------------------ PROPERTIES
 
             // define plugin name
-            var name = "mapify", obj = this,
+            var name = "mapify", obj = this, preZoomLeft = null, preZoomTop = null, preZoomZoomLevel = null,
             
             // define the defaults
             defaults = {
-             	  map_image:      "images/map.gif",
-             		up_image:       "images/up.gif",
-             		right_image:    "images/right.gif",
-             		down_image:     "images/down.gif",
-             	  left_image:     "images/left.gif",
-             	  center_image:   "images/center.gif",
-             	  plus_image:     "images/plus.gif",	
-             		minus_image:    "images/minus.gif",
-             		track_image:    "images/track.gif",
-             		slider_image:   "images/slider.gif",
-             		zoom:           2,
-             		zoom_levels:    3,
-             		zoom_sizes:     [[800,600], [1600,1200], [3200,2400]],
-             		coordinates:    [1200,600],
-             		distance:       256,
-             		speed:          500,
-             		debug:          true
-             	};
-            
+              map_image:      "images/map.gif",
+              up_image:       "images/up.gif",
+              right_image:    "images/right.gif",
+              down_image:     "images/down.gif",
+              left_image:     "images/left.gif",
+              center_image:   "images/center.gif",
+              plus_image:     "images/plus.gif",
+              minus_image:    "images/minus.gif",
+              track_image:    "images/track.gif",
+              slider_image:   "images/slider.gif",
+              zoom:           2,
+              zoom_levels:    3,
+              zoom_sizes:     [[800,600], [1600,1200], [3200,2400]],
+              coordinates:    [200,200],
+              distance:       256,
+              speed:          100,
+              debug:          true
+            };
+             	
             // ------------------------------ PRIVATE METHODS
             
             // draw legend method, in the upper left hand corner
@@ -103,9 +135,8 @@
                 
                 $("#down").bind("click", function(e) {
                   var next = ($("#map_image").position().top - defaults.distance) - offsetX;
-                  var bounds = ($("#map_image").height() - $("#map_image").parent().height()) * -1;
                   $("#map_image").animate({ 
-                    top: next >= bounds ? next : bounds,
+                    top: next >= getBounds()[1] ? next : getBounds()[1],
                   }, defaults.speed);
                 });
                 
@@ -119,23 +150,25 @@
                 
                 $("#right").bind("click", function(e) {
                   var next = ($("#map_image").position().left - defaults.distance) - offsetX;
-                  var bounds = ($("#map_image").width() - $("#map_image").parent().width()) * -1;
                   $("#map_image").animate({ 
-                    left: next >= bounds ? next : bounds,
+                    left: next >= getBounds()[0] ? next : getBounds()[0],
                   }, defaults.speed);
                 });
                 
                 $("#center").bind("click", function(e) {
-                  var leftOffset = (defaults.coordinates[0] > $("#map_image").parent().width() / 2) ? -1 : 1;
-                  var topOffset = (defaults.coordinates[1] >= $("#map_image").parent().height() / 2) ? -1 : 1;
+                  var leftOffset = (getCoordinates()[0] > $("#map_image").parent().width() / 2) ? -1 : 1;
+                  var topOffset = (getCoordinates()[1] >= $("#map_image").parent().height() / 2) ? -1 : 1;
+                  var left = (getCoordinates()[0] - $("#map_image").parent().width() / 2) * leftOffset;
+                  var top = (getCoordinates()[1] - $("#map_image").parent().height() / 2) * topOffset;
                   $("#map_image").animate({ 
-                    left: (defaults.coordinates[0] - $("#map_image").parent().width() / 2) * leftOffset,
-                    top: (defaults.coordinates[1] - $("#map_image").parent().height() / 2) * topOffset,
+                    left: left >= getBounds()[0] ? left : getBounds()[0],
+                    top: top >= getBounds()[1] ? top : getBounds()[1],
                   }, defaults.speed);
                 });
                 
                 $("#plus").bind("click", function(e) {
                   if (defaults.zoom < defaults.zoom_levels) {
+                    setCoordinates();
                     defaults.zoom++;
                     zoom();  
                   }
@@ -143,6 +176,7 @@
                 
                 $("#minus").bind("click", function(e) {
                   if (defaults.zoom > 1) {
+                    setCoordinates();
                     defaults.zoom--;
                     zoom();
                   }
@@ -151,6 +185,8 @@
             
             // draw map, adding image to the current DOM element
             function drawMap() {
+              // remove any previous image
+              $("#map_image").remove();
               var default_image = defaults.map_image.substring(0, defaults.map_image.indexOf('.')) + '_' + defaults.zoom + defaults.map_image.substring(defaults.map_image.indexOf('.'));
               var width = defaults.zoom_sizes[defaults.zoom - 1][0];
               var height = defaults.zoom_sizes[defaults.zoom - 1][1];
@@ -167,41 +203,60 @@
               	cursor: 'hand',
               	containment: $("#map_container")
               });
+              
+              // center on the passed coordinates
+              $("#center").click();
             };
             
             // draw a container element that will define the draggable bounds area
             function drawContainer() {
+              // remove any previous container
+              $("#map_container").remove();
               $("#" + elementID).before("<div id=\"map_container\"></div>");
-              
               // calculate width and height for container
               $("#map_container").width( $("#map_image").width() > $("#" + elementID).width() ? $("#map_image").width() + ($("#map_image").width() - $("#" + elementID).width()) : $("#" + elementID).width() );
               $("#map_container").height( $("#map_image").height() > $("#" + elementID).height() ? $("#map_image").height() + ($("#map_image").height() - $("#" + elementID).height()) : $("#" + elementID).height() );
-              
               // calculate position for container, used to restrict drag of #map_image
               var left = $("#" + elementID).position().left - (($("#map_container").width() - $("#" + elementID).width())/2);
               var top = $("#" + elementID).position().top - (($("#map_container").height() - $("#" + elementID).height())/2);
               $("#map_container").css({'position' : 'absolute'});
               $("#map_container").css({'left' : left});
               $("#map_container").css({'top' : top});
-              
               // resize contaier so image can't be dragged past x=0
               $("#map_container").width( $("#map_image").width() );
             }
-            
-            // define the offset (x and y) of the element being mapified
-            function defineOffset () {
-              // define the offset of the "map" element in the window
-              offsetX = $("#" + elementID).position().top;
-              offsetY = $("#" + elementID).position().left;
+
+            // define the pre-zoom coordinates
+            function setCoordinates() {
+              // get current coordinates
+              preZoomLeft = ($("#map_image").position().left * -1) + ($("#" + elementID).width()/2);
+              preZoomTop = ($("#map_image").position().top * -1) + ($("#" + elementID).height()/2);
+              preZoomZoomLevel = defaults.zoom;
             }
             
-            // load new image to mimic zoom
-            function  zoom() {
-              // load a new image
-              $("#map_image").remove();
-              $("#map_container").remove();
+            // convert coordinates post zoom
+            function getCoordinates() {
+              if (preZoomLeft != null) {
+                var widthDiff = defaults.zoom_sizes[defaults.zoom - 1][0] / defaults.zoom_sizes[preZoomZoomLevel - 1][0];
+                var heightDiff = defaults.zoom_sizes[defaults.zoom - 1][1] / defaults.zoom_sizes[preZoomZoomLevel - 1][1];
+                var postZoomLeft = preZoomLeft * widthDiff;
+                var postZoomTop = preZoomTop * heightDiff;
+                defaults.coordinates = [postZoomLeft, postZoomTop];
+              }
+              return defaults.coordinates;
+            }
+            
+            // get the left and top bounds, when moving and centering
+            function getBounds() {
+              var left = ($("#map_image").width() - $("#map_image").parent().width()) * -1;
+              var top = ($("#map_image").height() - $("#map_image").parent().height()) * -1;
+              return [left, top];
+            }
+            
+            // redraw the map using the new zoom level, manage zoom buttons
+            function zoom() {
+              // redraw the map with a new image, using the new zoom settings
               drawMap();
-              
               // disable buttons depending on zoom number
               if(defaults.zoom == 1) {
                 $("#minus").animate({ 
@@ -212,7 +267,6 @@
                   opacity: 1,
                 }, defaults.speed);
               }
-              
               if(defaults.zoom == defaults.zoom_levels) {
                 $("#plus").animate({ 
                   opacity: 0.5,
@@ -222,7 +276,13 @@
                   opacity: 1,
                 }, defaults.speed);
               }
-
+            }
+            
+            // define the offset (x and y) of the element being mapified, so we can offset the image of the map to match the edges
+            function setOffset () {
+              // define the offset of the "map" element in the window
+              offsetX = $("#" + elementID).position().top;
+              offsetY = $("#" + elementID).position().left;
             }
             
             // debug method, utilizing console or alerts
@@ -245,9 +305,9 @@
               return this.each(function() {
                 drawLegend();
                 drawMap();
-                defineOffset();
+                setOffset();
  			        });
- 			      } // end init
+ 			      }
              
         } // end mapify
 
